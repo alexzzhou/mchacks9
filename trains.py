32b621e9807a2ff1edba9dd47a1ce391286c20b6
Alex <alexzhou90@gmail.com>
2022-01-22 14:55:19 -0500

    updated on trains

--- trains.py
+++ trains.py
@@ -1,14 +1,42 @@
+import sys
 import pandas as pd
 import numpy as np
 
 class Train:
-    pass
+    def __init___(self, track, size):
+        self.track = track
+        self.size = size
+        self.time = 0
+
+    def skip(self):
+        if self.track != "A" or self.track != "B" or self.track != "C":
+            print('ERROR: cannot skip if not at station.')
+            return 1
+        self.time = 0
+
+    def increment(self):
+        self.time += 1
+        if self.track == "A" or self.track == "B" or self.track == "C":
+            if self.time == 3:
+                self
+
+    def __str__(self):
+        return f"Train at {self.track} \n \
+            size: {self.size}\
+            time: {self.time}"
 
 class L4Train(Train):
-    def __init__(self, station, size, )
+    def __init__(self):
+        self.track = "A"
+        self.size = 200
+        self.time = 0
+        
 
 class L8Train(Train):
-    pass
+    def __init__(self):
+        self.track = "A"
+        self.size = 400
+        self.time = 0
 
 class Station:
     pass
@@ -31,4 +59,8 @@ class PassengerGroup:
     def increment(self):
         self.wait_time += 1
 
+
+def increment():
+    
 main:
+
