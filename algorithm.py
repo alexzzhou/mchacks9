cd5a5c383652a3759a76993b304eda8e0b56773f
faithrts <faithruetas@gmail.com>
2022-01-31 01:45:15 -0500

    fixed the algorithm code

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,21 @@
 from this import d
 import pandas as pd
 
-output_file = pd.read_csv("~/desktop/mchacks9/output_format.csv")
+output_file = pd.read_csv("~/desktop/mchacks9/output_format.csv", index_col=False)
 passenger_arrivals = pd.read_csv("~/desktop/mchacks9/testing/rail_data.csv")
 
 A_arrivals = passenger_arrivals['A'].tolist()
 B_arrivals = passenger_arrivals['B'].tolist()
 C_arrivals = passenger_arrivals['C'].tolist()
 
-A_overflow = 0
-B_overflow = 0
-C_overflow = 0
+C_overflow = []
+
+for i in range(len(C_arrivals)):
+    C_overflow.append(0)
+
+A_last_stop = 0
+B_last_stop = 0
+C_last_stop = 0
 
 A_total = sum(A_arrivals)
 B_total = sum(B_arrivals)
@@ -42,71 +47,62 @@ def calculateTime(current_time):
 
     return time
 
-def arrivalsAtTime(cur_time, station):
-    
-    if(station == "A"):
-        if(cur_time == 0):
-            return A_arrivals[0]
-        elif(str(cur_time)[-1] == "0"):
-            return A_arrivals[(cur_time - 1)//10]
-        else:
-            return A_arrivals[cur_time//10]
+def sum(list, start, stop):
+    sum = 0
+
+    for i in range(start, stop):
+        sum += list[i]
+
+    return sum
+
+def calculatePassengers(cur_time, station):
+    global A_last_stop, B_last_stop, C_last_stop
+
+    start = 0
+    stop = 0
+
+    if(cur_time != 0 and str(cur_time)[-1] == "0"):
+        stop = (cur_time - 1)//10
+    else:
+        stop = cur_time//10
+
+    if(stop >= 19):
+            stop = 18
         
+    if(station == "A"):
+        start = A_last_stop 
+        A_last_stop = stop + 1 
+        
+        return sum(A_arrivals, start, stop + 1)
+
     elif(station == "B"):
-        if(cur_time == 0):
-            return B_arrivals[0]
-        elif(str(cur_time)[-1] == "0"):
-            return B_arrivals[(cur_time - 1)//10]
-        else:
-            return B_arrivals[cur_time//10]
+        start = B_last_stop
+        B_last_stop = stop + 1
+        return sum(B_arrivals, start, stop + 1)
 
     elif(station == "C"):
-        if(cur_time == 0):
-            return C_arrivals[0]
-        elif(str(cur_time)[-1] == "0"):
-            return C_arrivals[(cur_time - 1)//10]
-        else:
-            return C_arrivals[cur_time//10]
+        start = C_last_stop
+        C_last_stop = stop + 1
+        return sum(C_arrivals, start, stop + 1)
 
-def clearStation(cur_time, station):
-    if(station == "A"):
-        if(cur_time == 0):
-            A_arrivals[0] = 0
-        elif(str(cur_time)[-1] == "0"):
-            A_arrivals[(cur_time - 1)//10] = 0
-        else:
-            A_arrivals[cur_time//10] = 0
+def calculateOverflowPassengers(cur_time):
+    global A_last_stop, B_last_stop, C_last_stop
+    global C_overflow
+
+    start = 0
+    stop = 0
+
+    if(cur_time != 0 and str(cur_time)[-1] == "0"):
+        stop = ((cur_time - 1)//10) + 1
+    else:
+        stop = (cur_time//10) + 1
         
-    elif(station == "B"):
-        if(cur_time == 0):
-            B_arrivals[0] = 0
-        elif(str(cur_time)[-1] == "0"):
-            B_arrivals[(cur_time - 1)//10] = 0
-        else:
-            B_arrivals[cur_time//10] = 0
+    start = stop - 1
+    passengers = sum(C_overflow, start, stop + 1)
 
-    elif(station == "C"):
-        if(cur_time == 0):
-            C_arrivals[0] = 0
-        elif(str(cur_time)[-1] == "0"):
-            C_arrivals[(cur_time - 1)//10] = 0
-        else:
-            C_arrivals[cur_time//10] = 0
+    C_arrivals[start + 1] -= passengers
 
-def updateOverflow(cur_time, station):
-    global A_overflow, B_overflow, C_overflow
-
-    if(station == "A"):
-        A_overflow += arrivalsAtTime(cur_time, "A")
-        clearStation(cur_time, "A")
-
-    elif (station == "B"):
-        B_overflow += arrivalsAtTime(cur_time, "B")
-        clearStation(cur_time, "B")
-
-    elif (station == "C"):
-        C_overflow += arrivalsAtTime(cur_time, "C")
-        clearStation(cur_time, "C")
+    return passengers
 
 def addTrain(cur_time, train_type, skips):
         global L4Trains, L8Trains
@@ -144,27 +140,24 @@ def addTrain(cur_time, train_type, skips):
             cur_time += 3
             b_arrival = a_arrival + 8 + 3
 
-            passengers_at_station = arrivalsAtTime(cur_time, "A") + A_overflow
+            passengers_at_station = calculatePassengers(cur_time, "A")
 
             # if available cap is less than equal to passengers wanting a ride
             if(a_avail_cap <= passengers_at_station):
                 a_boarding = a_avail_cap
-                A_overflow = passengers_at_station - a_boarding
+
+                # overflow goes to the next time slot
+                A_arrivals[A_last_stop] += passengers_at_station - a_boarding
 
             # if available cap is greater than passengers wanting a ride
             else:
                 a_boarding = passengers_at_station
-                A_overflow = 0
 
             # updating number of people waiting
             A_remaining -= a_boarding
         else:
-            A_overflow += arrivalsAtTime(cur_time, "A")
+            A_arrivals[A_last_stop] += passengers_at_station - a_boarding
             b_arrival = a_arrival + 8
-        #
-        clearStation(cur_time, "A")
-        updateOverflow(cur_time, "B")
-        updateOverflow(cur_time, "C")
         #
         b_avail_cap = a_avail_cap - a_boarding
         cur_time += 8
@@ -174,26 +167,23 @@ def addTrain(cur_time, train_type, skips):
             cur_time += 3
             c_arrival = b_arrival + 9 + 3
 
-            passengers_at_station = arrivalsAtTime(cur_time, "B") + B_overflow
+            passengers_at_station = calculatePassengers(cur_time, "B")
 
             # if available cap is less than equal to passengers wanting a ride
             if(b_avail_cap <= passengers_at_station):
                 b_boarding = b_avail_cap
-                B_overflow = passengers_at_station - b_boarding
+                
+                # overflow goes to the next time slot
+                B_arrivals[B_last_stop] += passengers_at_station - b_boarding
 
             # if available cap is greater than passengers wanting a ride
             else:
                 b_boarding = passengers_at_station
-                B_overflow = 0
 
             # updating number of people waiting
             B_remaining -= b_boarding
         else:
-            B_overflow += arrivalsAtTime(cur_time, "B")
             c_arrival = b_arrival + 9
-        #
-        clearStation(cur_time, "B")
-        updateOverflow(cur_time, "C")
         #
         c_avail_cap = b_avail_cap - b_boarding
         cur_time += 9
@@ -203,26 +193,97 @@ def addTrain(cur_time, train_type, skips):
             cur_time += 3
             u_arrival = c_arrival + 11 + 3
 
-            passengers_at_station = arrivalsAtTime(cur_time, "C") + C_overflow
+            passengers_at_station = calculatePassengers(cur_time, "C")
 
             # if available cap is less than equal to passengers wanting a ride
             if(c_avail_cap <= passengers_at_station):
                 c_boarding = c_avail_cap
-                C_overflow = passengers_at_station - c_boarding
+                
+                # overflow goes to the next time slot
+                C_arrivals[C_last_stop] += passengers_at_station - c_boarding
+                C_overflow[C_last_stop - 1] += passengers_at_station - c_boarding
 
             # if available cap is greater than passengers wanting a ride
             else:
                 c_boarding = passengers_at_station
-                C_overflow = 0
 
             # updating the number of people waiting
             C_remaining -= c_boarding
         else:
-            C_overflow += arrivalsAtTime(cur_time, "C")
             u_arrival = c_arrival + 11
         #
-        clearStation(cur_time, "C")
-        #
+        u_avail_cap = c_avail_cap - c_boarding
+        cur_time += 11
+        u_offboarding = a_boarding + b_boarding + c_boarding
+
+        tempDF = pd.DataFrame([[train_count + 1, train_type, calculateTime(a_arrival), a_avail_cap, a_boarding, calculateTime(b_arrival), b_avail_cap, b_boarding, calculateTime(c_arrival), c_avail_cap, c_boarding, calculateTime(u_arrival), u_avail_cap, u_offboarding]])
+       
+        # for testing purposes
+        # print('\n'.join(tempDF.to_string(index = False).split('\n')[1:]))
+
+        tempDF.columns = ['TrainNum', 'TrainType', 'A_ArrivalTime', 'A_AvailCap', 'A_Boarding', 'B_ArrivalTime', 'B_AvailCap', 'B_Boarding', 'C_ArrivalTime', 'C_AvailCap', 'C_Boarding', 'U_Arrival', 'U_AvailCap', 'U_Offloading']
+        output_file = output_file.append(tempDF, ignore_index=True)
+
+        train_count += 1
+
+def addOverflowTrain(cur_time, train_type):
+        global L4Trains, L8Trains
+        global A_remaining, B_remaining, C_remaining
+        global A_overflow, B_overflow, C_overflow
+        global output_file
+        global train_count
+
+        passengers_at_station = 0
+
+        if(train_type == "L4"):
+            L4Trains -= 1
+            a_avail_cap = 200
+        else:
+            L8Trains -= 1
+            a_avail_cap = 400
+
+        a_arrival = cur_time
+        a_boarding = 0
+
+        b_arrival = 0
+        b_avail_cap = a_avail_cap
+        b_boarding = 0
+
+        c_arrival = 0
+        c_avail_cap = b_avail_cap
+        c_boarding = 0
+
+        u_arrival = 0
+        u_avail_cap = c_avail_cap
+        u_boarding = 0
+
+        # at station A
+        b_arrival = a_arrival + 8
+        cur_time += 8
+
+        # at station B
+        c_arrival = b_arrival + 9
+        cur_time += 9
+
+        # at station C
+        cur_time += 3
+        u_arrival = c_arrival + 11 + 3
+        passengers_at_station = calculateOverflowPassengers(cur_time)
+
+        # if available cap is less than equal to passengers wanting a ride
+        if(c_avail_cap <= passengers_at_station):
+            c_boarding = c_avail_cap
+                
+            # overflow goes to the next time slot
+            C_arrivals[C_last_stop] += passengers_at_station - c_boarding
+
+        # if available cap is greater than passengers wanting a ride
+        else:
+            c_boarding = passengers_at_station
+
+            # updating the number of people waiting
+            C_remaining -= c_boarding
+
         u_avail_cap = c_avail_cap - c_boarding
         cur_time += 11
 
@@ -230,20 +291,21 @@ def addTrain(cur_time, train_type, skips):
 
         tempDF = pd.DataFrame([[train_count + 1, train_type, calculateTime(a_arrival), a_avail_cap, a_boarding, calculateTime(b_arrival), b_avail_cap, b_boarding, calculateTime(c_arrival), c_avail_cap, c_boarding, calculateTime(u_arrival), u_avail_cap, u_offboarding]])
        
-        print('\n'.join(tempDF.to_string(index = False).split('\n')[1:]))
-
+        # for testing purposes
+        # print('\n'.join(tempDF.to_string(index = False).split('\n')[1:]))
 
         tempDF.columns = ['TrainNum', 'TrainType', 'A_ArrivalTime', 'A_AvailCap', 'A_Boarding', 'B_ArrivalTime', 'B_AvailCap', 'B_Boarding', 'C_ArrivalTime', 'C_AvailCap', 'C_Boarding', 'U_Arrival', 'U_AvailCap', 'U_Offloading']
         output_file = output_file.append(tempDF, ignore_index=True)
 
         train_count += 1
-
+ 
 def main():
 
     global A_overflow, B_overflow, C_overflow
     global A_total, B_total, C_total
     global L4Trains, L8Trains
     global train_count
+    global A_last_stop, B_last_stop, C_last_stop
     
     overflow = 0
     cur_time = 0
@@ -267,15 +329,16 @@ def main():
         # if the current time ends with minute 8
         if(str(cur_time)[-1] == "8"):
 
-            if(cur_time == 58):
-                hi = 5
+            A_pickup = calculatePassengers(cur_time + 3, "A")
+            B_pickup = calculatePassengers(cur_time + 3 + 11 + 3, "B")
+            C_pickup = calculatePassengers(cur_time + 3 + 11 + 3 + 12 + 3, "C")
 
-            A_pickup = arrivalsAtTime(cur_time + 3, "A")
-            B_pickup = arrivalsAtTime(cur_time + 3 + 11 + 3, "B")
-            C_pickup = arrivalsAtTime(cur_time + 3 + 11 + 3 + 12 + 3, "C")
-            
+            A_last_stop -= 1
+            B_last_stop -= 1
+            C_last_stop -= 1
+
             # calculate potential overflow
-            overflow = (A_overflow + B_overflow + C_overflow + A_pickup + B_pickup + C_pickup) - 400
+            overflow = (A_pickup + B_pickup + C_pickup) - 400
 
             # send a "standard" train
             addTrain(cur_time, "L8", [])
@@ -285,11 +348,11 @@ def main():
 
                 # if there are still L4 trains left
                 if(L4Trains > 0):
-                    addTrain(cur_time + 9, "L4", ["A", "B"])
+                    addOverflowTrain(cur_time + 9, "L4")
 
                 # else, send an L8 train
                 else:
-                    addTrain(cur_time + 9, "L8", ["A", "B"])
+                    addOverflowTrain(cur_time + 9, "L8")
 
         # incrementing time
         cur_time += 2
@@ -349,7 +412,7 @@ def main():
         else:
             addTrain(send_time, "L4", [])
 
-    print(send_time)
+    cur_time = send_time
         
     for group2 in range(1, num_large_groups + 1):
         send_time = cur_time + ((10 * (large_group_size)) * group2)
@@ -360,7 +423,11 @@ def main():
         else:
             addTrain(send_time, "L4", [])
 
+    # uncomment the line below to view the resulting dataframe (but with index column)
     # print(output_file)
+
+    # uncomment the line below to convert the dataframe to a csv
+    # output_file.to_csv(index=False)
 
 if __name__ == "__main__":
     main()
